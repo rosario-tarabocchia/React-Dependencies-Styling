a302ab3461c531209ad61fcaed13032f207d163f
Rosario Tarabocchia <84477640+rosario-tarabocchia@users.noreply.github.com>
2022-04-21 19:18:56 -0400

    Lesson Complete

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+  const [isExpanded, setExpanded] = useState(false);
+  // const [isHidden, setIsHidden] = useState(true);
+  // const [numberOfRows, setNumberOfRows] = useState("1");
+
+  // function clickedInBox() {
+  //   setIsHidden(false);
+  //   setNumberOfRows("3");
+  // }
+
+  function expand() {
+    setExpanded(true);
+  }
 
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
         [name]: value
@@ -23,26 +38,37 @@ function CreateArea(props) {
       title: "",
       content: ""
     });
+    // setIsHidden(true);
+    // setNumberOfRows("1");
     event.preventDefault();
   }
 
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {isExpanded && (
+          <input
+            // hidden={isHidden}
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
         <textarea
+          onClick={expand}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddCircleOutlineIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <StickyNote2Icon />
+        Keeper
+      </h1>
     </header>
   );
 }
